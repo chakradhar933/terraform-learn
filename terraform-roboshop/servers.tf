1eab3214835837a0283847ecc561261e1d37c912
chakradhar <chakradhar.thatikonda933@gmail.com>
2023-05-02 18:59:21 +0530

    data-resources

--- terraform-roboshop/servers.tf
+++ terraform-roboshop/servers.tf
@@ -3,8 +3,9 @@ data "aws_ami" "centos" {
   most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice"
 }
+
 output "ami" {
-    value= data.aws_ami.centos.image_id
+    value = data.aws_ami.centos.image_id
 }
 
 
