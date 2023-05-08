ee878d1f3cf5809b0fe409196e7463a297241d1d
chakradhar <chakradhar.thatikonda933@gmail.com>
2023-05-08 19:09:00 +0530

    mongodb

--- terraform-roboshop/data-outputs/servers.tf
+++ terraform-roboshop/data-outputs/servers.tf
@@ -40,7 +40,7 @@ resource "aws_instance" "mongodb" {
   }
 }
 output "mongodb" {
-    value = aws_instance.mongodb.public_ip
+    value = aws_instance.mongodb.instance_type.public_ip
 }
 resource "aws_route53_record" "mongodb" {
   zone_id = "Z0246811XH3HRK0AWHVN"
